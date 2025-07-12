455e544a619bcfdc58e91d8f89983036efab9289
amir <amirtaki623@gmail.com>
2025-07-12 22:06:37 +0330

    repsonses Farewll  js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ const messageList = document.querySelector(".chat-box") ;
 const input = document.querySelector('.chat-input input');
 const sendButton = document.querySelector(".chat-input button")
 
-const response = {
+const responses = {
     Greeting: [
         "Hello there!",
         "Hi! How can I help you today?"
+    ],
+    Farewell : [
+      "Good bye!",
+      "Bye! Have a great day!",  
     ],
 
 }
