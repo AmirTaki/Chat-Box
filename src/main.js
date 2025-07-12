0bcd98ee1908da01c763a59cb864212dea5b5fd7
amir <amirtaki623@gmail.com>
2025-07-12 22:04:14 +0330

    querySelector js

---
+++ src/main.js
@@ -0,0 +1,3 @@
+const messageList = document.querySelector(".chat-box") ;
+const input = document.querySelector('.chat-input input');
+const sendButton = document.querySelector(".chat-input button")
